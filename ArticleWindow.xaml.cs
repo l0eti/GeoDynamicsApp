1c8c4f24d3b8a3a31e6518ebba4219384dedf829
Andrew <kezin636@gmail.com>
2024-11-02 12:31:35 +0300

    добавлены новости, статьи, а так же небольшие изменения в дизайне

---
+++ ArticleWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GeoDynamicsApp
+{
+    /// <summary>
+    /// Interaction logic for ArticleWindow.xaml
+    /// </summary>
+    public partial class ArticleWindow : Window
+    {
+        private Article _article;
+
+        public ArticleWindow(Article article)
+        {
+            InitializeComponent();
+            _article = article;
+
+            if (_article == null)
+            {
+                MessageBox.Show(
+                    "Задайте статью объектом Article!",
+                    "Демо",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Warning
+                );
+
+                return;
+            }
+            
+            Title = _article.Title;
+            TitleTextBlock.Text = _article.Title;
+            ContentsTextBlock.Text = _article.Contents;
+        }
+    }
+}
